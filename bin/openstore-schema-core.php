ae39b73251bf2c9c316466df1ca6a490af93d9a7
Sébastien Vanvelthem <belgattitude@gmail.com>
2015-12-21 12:10:10 +0100

    Console

---
+++ bin/openstore-schema-core.php
@@ -0,0 +1,68 @@
+<?php
+
+// Bootstrap
+try {
+    // Step 1: init autoloader
+    if (!file_exists(__DIR__.'/../vendor/autoload.php')) {
+        throw new \Exception('Cannot find composer vendor autoload, run composer update');
+    } else {
+        $loader = require __DIR__.'/../vendor/autoload.php';
+    }
+
+    // Step 2 : init configuration
+    $config_file = realpath(dirname(__FILE__) . "/../config/") . DIRECTORY_SEPARATOR . 'openstore-schema-core.config.php';
+    if (!file_exists($config_file)) {
+        throw new \Exception("Cannot find configuration file '$config_file'");
+    }
+    $config = include $config_file;
+    if (!$config) {
+        throw new \Exception("Cannot parse or empty configuration file '$config_file'");
+    }
+} catch (\Exception $e) {
+    echo $e->getMessage() . "\n";
+    exit(1);
+}
+
+$setup = new Openstore\Schema\Core\Doctrine\Setup($config['database'], $config['paths'], $config['namespace']);
+$setup->setEnvironment($config['env']);
+
+$cli = new Symfony\Component\Console\Application('openstore-schema-core console', '1.0.0');
+$cli->setCatchExceptions(true);
+// commands
+$cli->addCommands(array(
+    // DBAL Commands
+    new Doctrine\DBAL\Tools\Console\Command\RunSqlCommand(),
+    new Doctrine\DBAL\Tools\Console\Command\ImportCommand(),
+
+    // ORM Commands
+    new Doctrine\ORM\Tools\Console\Command\ClearCache\MetadataCommand(),
+    new Doctrine\ORM\Tools\Console\Command\ClearCache\ResultCommand(),
+    new Doctrine\ORM\Tools\Console\Command\ClearCache\QueryCommand(),
+    new Doctrine\ORM\Tools\Console\Command\SchemaTool\CreateCommand(),
+    new Doctrine\ORM\Tools\Console\Command\SchemaTool\UpdateCommand(),
+    new Doctrine\ORM\Tools\Console\Command\SchemaTool\DropCommand(),
+    new Doctrine\ORM\Tools\Console\Command\EnsureProductionSettingsCommand(),
+    new Doctrine\ORM\Tools\Console\Command\ConvertDoctrine1SchemaCommand(),
+    new Doctrine\ORM\Tools\Console\Command\GenerateRepositoriesCommand(),
+    new Doctrine\ORM\Tools\Console\Command\GenerateEntitiesCommand(),
+    new Doctrine\ORM\Tools\Console\Command\GenerateProxiesCommand(),
+    new Doctrine\ORM\Tools\Console\Command\ConvertMappingCommand(),
+    new Doctrine\ORM\Tools\Console\Command\RunDqlCommand(),
+    new Doctrine\ORM\Tools\Console\Command\ValidateSchemaCommand(),
+    new Doctrine\ORM\Tools\Console\Command\InfoCommand(),
+));
+
+$em = $setup->getEntityManager();
+
+// helpers
+$helpers = array(
+    'db' => new Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper($em->getConnection()),
+    'em' => new Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($em),
+);
+foreach ($helpers as $name => $helper) {
+    $cli->getHelperSet()->set($helper, $name);
+}
+
+$cli->run();
+//return $cli;
+
---
+++ src/Openstore/Schema/Core/Doctrine/Setup.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace Openstore\Schema\Core\Doctrine;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Configuration;
+use Doctrine\Common\Cache as DoctrineCache;
+use Doctrine\ORM\Tools\Setup as DoctrineSetup;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\CachedReader;
+use Gedmo;
+use Doctrine\Common\Persistence\Mapping\Driver\MappingDriverChain;
+use Doctrine\Common\EventManager;
+
+class Setup {
+
+    /**
+     * @var array
+     */
+    protected $dbParams;
+    
+    /**
+     * Paths to store entities
+     * @var array
+     */
+    protected $paths;
+    
+    
+    /**
+     * Entity namespace
+     * @var string
+     */
+    protected $namespace;
+
+    /**
+     *
+     * @var string development|production
+     */
+    protected $env;
+    
+    /**
+     * Constructor
+     * @param array $dbParams database connection configuration
+     * @param array $paths paths to look for entities
+     * @param string $namespace entity namespace
+     */
+    function __construct(array $dbParams, array $paths, $namespace) {
+        $this->dbParams = $dbParams;
+        $this->paths = $paths;
+        $this->namespace = $namespace;
+    }
+
+    /**
+     * @var string production|development
+     */
+    function setEnvironment($env) {
+        $this->env = $env;
+    }
+    
+    
+    /**
+     * 
+     * @return EntityManager
+     */
+    function getEntityManager() {
+
+        
+        $cache = new DoctrineCache\ArrayCache();
+
+        $annotationReader = new AnnotationReader();
+        $cachedAnnotationReader = new CachedReader(
+                $annotationReader,
+                $cache 
+        );
+        
+        // create a driver chain for metadata reading
+        $driverChain = new MappingDriverChain();
+        
+        // load superclass metadata mapping only, into driver chain
+        // also registers Gedmo annotations.NOTE: you can personalize it
+        Gedmo\DoctrineExtensions::registerAbstractMappingIntoDriverChainORM(
+                $driverChain, // our metadata driver chain, to hook into
+                $cachedAnnotationReader // our cached annotation reader
+        );
+
+        // now we want to register our application entities,
+        // for that we need another metadata driver used for Entity namespace
+        $annotationDriver = new AnnotationDriver(
+                $cachedAnnotationReader, // our cached annotation reader
+                $this->paths
+        );
+        
+        
+        $driverChain->addDriver($annotationDriver, $this->namespace);
+
+        // general ORM configuration
+        
+        $isDevMode = $this->env != "production";
+        $config = DoctrineSetup::createAnnotationMetadataConfiguration($this->paths, $isDevMode);
+        $config->setMetadataCacheImpl($cache);
+        $config->setQueryCacheImpl($cache);
+        $config->setMetadataDriverImpl($driverChain);
+        $config->setProxyDir(sys_get_temp_dir());
+        $config->setProxyNamespace($this->namespace . '\Proxy');
+        $config->setAutoGenerateProxyClasses($isDevMode); 
+        
+
+        // Third, create event manager and hook prefered extension listeners
+        $evm = new EventManager();
+        
+        // gedmo extension listeners
+        // sluggable
+        $sluggableListener = new Gedmo\Sluggable\SluggableListener();
+        // you should set the used annotation reader to listener, to avoid creating new one for mapping drivers
+        $sluggableListener->setAnnotationReader($cachedAnnotationReader);
+        $evm->addEventSubscriber($sluggableListener);
+
+        // tree
+        $treeListener = new Gedmo\Tree\TreeListener();
+        $treeListener->setAnnotationReader($cachedAnnotationReader);
+        $evm->addEventSubscriber($treeListener);
+
+        // loggable, not used in example
+        $loggableListener = new Gedmo\Loggable\LoggableListener;
+        $loggableListener->setAnnotationReader($cachedAnnotationReader);
+        $loggableListener->setUsername('unknown');
+        $evm->addEventSubscriber($loggableListener);
+        
+        // timestampable
+        $timestampableListener = new Gedmo\Timestampable\TimestampableListener();
+        $timestampableListener->setAnnotationReader($cachedAnnotationReader);
+        $evm->addEventSubscriber($timestampableListener);
+
+        // blameable
+        $blameableListener = new Gedmo\Blameable\BlameableListener();
+        $blameableListener->setAnnotationReader($cachedAnnotationReader);
+        $blameableListener->setUserValue('unknown'); // determine from your environment
+        $evm->addEventSubscriber($blameableListener);
+
+        // translatable - buggy !!!
+        /*
+        $translatableListener = new Gedmo\Translatable\TranslatableListener();
+        // current translation locale should be set from session or hook later into the listener
+        // most important, before entity manager is flushed
+        $translatableListener->setTranslatableLocale('en');
+        $translatableListener->setDefaultLocale('en');
+        $translatableListener->setAnnotationReader($cachedAnnotationReader);
+        $evm->addEventSubscriber($translatableListener);
+        */
+        
+        // sortable, not used in example
+        $sortableListener = new Gedmo\Sortable\SortableListener;
+        $sortableListener->setAnnotationReader($cachedAnnotationReader);
+        $evm->addEventSubscriber($sortableListener);
+
+        // mysql set names UTF-8 if required
+        $evm->addEventSubscriber(new \Doctrine\DBAL\Event\Listeners\MysqlSessionInit());
+        
+        // Finally, create entity manager
+        return EntityManager::create($this->dbParams, $config, $evm);
+    }
+
+}
